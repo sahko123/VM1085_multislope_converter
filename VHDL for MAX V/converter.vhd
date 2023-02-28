b0887c61c44cd077a7a85822b537376e0e4373b8
sahko123 <sahko247@gmail.com>
2023-02-28 00:01:04 +0000

    counter added as component

--- VHDL for MAX V/converter.vhd
+++ VHDL for MAX V/converter.vhd
@@ -7,50 +7,98 @@ port(
 CLK:in std_logic;
 data:out std_logic_vector(7 downto 0);
 address:in std_logic_vector(3 downto 0);
-SW10K1,SW10k2,SW80k2,SW640K3,SW5120K4:out std_logic;
-COMP,RUNUP:in std_logic
+SW_sample,SW10K1,SW10k2,SW80k3,SW640K4,SW5120K5:out std_logic:='0';
+COMP,RUN_UPorDOWN,CONV:in std_logic
 
 );
 end entity;
+
+
+
+
 
 architecture behavioral of converter is
 
 
 signal RP_COUNT:signed(23 downto 0):=(others=>'0');--runup counter
-signal count_stage1:unsigned(7 downto 0):=(others=>'0');--10k rundown
-signal count_stage23:unsigned(7 downto 0):=(others=>'0');--80k pos(4MSB) 640k neg(4LSB)
-signal count_stage4:unsigned(7 downto 0):=(others=>'0');--5.12M pos
+signal count_stage12:unsigned(7 downto 0):=(others=>'0');--10k rundown
+signal count_stage34:unsigned(7 downto 0):=(others=>'0');--80k pos(4MSB) 640k neg(4LSB)
+signal count_stage5:unsigned(7 downto 0):=(others=>'0');--5.12M pos
+signal state:unsigned(3 downto 0):=(others=>'0');
+signal timer:std_logic_vector(3 downto 0);
+signal timer_reset:std_logic;
 
+component counter is
+port(
+count_out:out std_logic_vector(3 downto 0);
+RST,CLK:in std_logic:='0'
+);
+end component;
 begin
 
-
+time_counter:counter port map(count_out=>timer,CLK=>CLK,RST=>timer_reset);
 
 --control registers
 process(address) begin
 	case address is
-		when "0000" => data<=std_logic_vector(count_stage1);
-		when "0001" => data<=std_logic_vector(count_stage23);
-		when "0010" => data<=std_logic_vector(count_stage4);
+		when "0000" => data<=std_logic_vector(count_stage12);
+		when "0001" => data<=std_logic_vector(count_stage34);
+		when "0010" => data<=std_logic_vector(count_stage5);
 		when "0011" => data<=std_logic_vector(RP_COUNT)(7 downto 0);
 		when "0100" => data<=std_logic_vector(RP_COUNT)(15 downto 8);
 		when "0101" => data<=std_logic_vector(RP_COUNT)(23 downto 16);
-		when "1001" => data<="01010101";
+		when "0110" => data<="01010101";
 		when others => data<=(others=>'Z');
 	end case;
 end process;
 
 --conversion process
-process(CLK) begin
 
-if rising_edge(CLK) then
-RP_COUNT<=RP_COUNT+1;
-count_stage1<=count_stage1+1;
-count_stage23<=count_stage23+1;
-count_stage4<=count_stage4+1;
 
+
+process(clk) begin
+
+
+case state is
+
+
+when "0000" =>					--initial state;
+SW_sample<='0';
+SW10K1<='0';
+SW10K2<='0';
+SW80K3<='0';
+SW640K4<='0';
+SW5120K5<='0';
+timer_reset<='1';
+if CONV='1' then
+state<="0001";
 end if;
 
+
+
+when "0001"=>					--t1 sample
+
+SW_sample<='1'; --enable sample switch
+if timer>="0111" then
+timer_reset<='1';
+state<="0010";
+end if;
+
+
+when "0010"=>					--runup
+SW_sample<='0';
+
+when "0011"=>
+
+
+when "0100"=>
+
+
+
+when others =>state<="0000";
+end case;
 end process;
+
 
 
 
---
+++ VHDL for MAX V/counter.vhd
@@ -0,0 +1,26 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+entity counter is
+port(
+count_out:out std_logic_vector(3 downto 0);
+RST,CLK:in std_logic:='0'
+);
+end entity;
+
+architecture behavioral of counter is
+signal count:unsigned(3 downto 0);
+begin
+count_out<=std_logic_vector(count);
+
+process(clk) begin
+if rising_edge(clk) then
+count<=count+1; 
+end if;
+if RST='1' then
+count<="0000";
+end if;
+end process;
+
+
+end behavioral;
