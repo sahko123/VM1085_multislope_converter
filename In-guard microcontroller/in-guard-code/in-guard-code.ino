e344e9f5bb0b50cf9cac35405fa8d6cd1e691cef
sahko123 <sahko247@gmail.com>
2023-03-01 14:14:38 +0000

    added write and read to cpld in inguard code

--- In-guard microcontroller/in-guard-code/in-guard-code.ino
+++ In-guard microcontroller/in-guard-code/in-guard-code.ino
@@ -39,7 +39,7 @@ byte read_cpld(byte address){
   if(bitRead(address,3)==1){digitalWrite(add_3,1);}
   else{digitalWrite(add_3,0);}
   //read data
-  delayMicroseconds(10);
+  delayMicroseconds(5);
   bitWrite(data,0,digitalRead(dat_0));
   bitWrite(data,1,digitalRead(dat_1));
   bitWrite(data,2,digitalRead(dat_2));
@@ -51,7 +51,35 @@ byte read_cpld(byte address){
   return data;
   }
   
-byte write_cpld(int address,byte data){
+byte write_cpld(byte address,byte data){
+  //set data pinmodes
+  pinMode(dat_0,OUTPUT);
+  pinMode(dat_1,OUTPUT);
+  pinMode(dat_2,OUTPUT);
+  pinMode(dat_3,OUTPUT);
+  pinMode(dat_4,OUTPUT);
+  pinMode(dat_5,OUTPUT);
+  pinMode(dat_6,OUTPUT);
+  pinMode(dat_7,OUTPUT);
+  //write address
+  if(bitRead(address,0)==1){digitalWrite(add_0,1);}
+  else{digitalWrite(add_0,0);}
+  if(bitRead(address,1)==1){digitalWrite(add_1,1);}
+  else{digitalWrite(add_1,0);}
+  if(bitRead(address,2)==1){digitalWrite(add_2,1);}
+  else{digitalWrite(add_2,0);}
+  if(bitRead(address,3)==1){digitalWrite(add_3,1);}
+  else{digitalWrite(add_3,0);}
+  //write data
+  delayMicroseconds(5);
+  digitalWrite(dat_0,bitRead(data,0));
+  digitalWrite(dat_1,bitRead(data,1));
+  digitalWrite(dat_2,bitRead(data,2));
+  digitalWrite(dat_3,bitRead(data,3));
+  digitalWrite(dat_4,bitRead(data,4));
+  digitalWrite(dat_5,bitRead(data,5));
+  digitalWrite(dat_6,bitRead(data,6));
+  digitalWrite(dat_7,bitRead(data,7));
   
   }
   
