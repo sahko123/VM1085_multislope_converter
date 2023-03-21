4bbeb6e4da8165718778358db34dead7bcb29b7c
sahko123 <sahko247@gmail.com>
2023-03-21 01:50:16 +0000

    added relay and calibration functions

--- In-guard microcontroller/in-guard-code/in-guard-code.ino
+++ In-guard microcontroller/in-guard-code/in-guard-code.ino
@@ -12,18 +12,41 @@ int dat_4=8;
 int dat_5=9;
 int dat_6=10;
 int dat_7=11;
-
+//relays
+int zero=17;
+int ref_input=16;
+//cpld pins
 int conv=12;
 int data_ready=13;
 
-double stime=1;
+double stime=0.2;
 double clk=10000000;
 double samplecount=clk*stime;
-double ref=10.0;
-double zerocal=0;
-double gain=ref/-0.551309426020
-;
+double vref=10.0169870399;
+double zerocal=0.0;
+double ext_zerocal=-0.0554799688;
+double gain=-1.8098457364;
 
+int running_average=0;
+int average_samples=10;
+double rolling=0;
+
+void average_1(double reading){
+  if(running_average==0){rolling=reading;}
+  else if(running_average<average_samples){rolling=(rolling+reading)/2;}
+  else{running_average=0;}
+  running_average+=1;
+  }
+
+void toggle_relay(int relay){
+  pinMode(relay,OUTPUT);
+  if(digitalRead(relay)==HIGH){digitalWrite(relay,LOW);}
+  else{digitalWrite(relay,HIGH);}
+  }
+void set_relay(int relay,bool setting){
+  pinMode(relay,OUTPUT);
+  digitalWrite(relay,setting);
+  }
 byte read_id(){
   byte out=read_cpld(0b00001000);
   return out;
@@ -91,46 +114,90 @@ void write_cpld(byte address,byte data){
   digitalWrite(dat_5,bitRead(data,5));
   digitalWrite(dat_6,bitRead(data,6));
   digitalWrite(dat_7,bitRead(data,7));
-  
   }
 long get_runup_read(){
   long reading;
-  int lsb=read_cpld(0b00000010)<<16;
+  int msb=read_cpld(0b00000011)<<24;
+  int mmsb=read_cpld(0b00000010)<<16;
   int mlsb=read_cpld(0b00000001)<<8;
-  int msb=read_cpld(0b00000000);
-  reading=lsb+mlsb+msb;
+  int lsb=read_cpld(0b00000000);
+  reading=lsb+mlsb+mmsb+msb;
   if(reading>8388608){
     reading=reading-16777216;}
   return reading;
   }
 float read_run1(){
-  float reading=read_cpld(0b00000011);
-  return reading;
-}
-float read_run2(){
   float reading=read_cpld(0b00000100);
   return reading;
 }
-float read_run3(){
+float read_run2(){
   float reading=read_cpld(0b00000101);
   return reading;
 }
-float read_run4(){
+float read_run3(){
   float reading=read_cpld(0b00000110);
+  return reading;
+}
+float read_run4(){
+  float reading=read_cpld(0b00000111);
   return reading;
   } 
 float read_run5(){
-  float reading=read_cpld(0b00000111);
+  float reading=read_cpld(0b00001000);
   return reading;
   }
-double raw_reading(double stime){
-  double reading=((((10.0*get_runup_read())+(read_run1())-(read_run2())+(read_run3()/4.1)-(read_run4()/38.0)+(read_run5()/260)))/samplecount);
-  return reading;
+void adc_gain(){
+  set_relay(zero,true);
+  set_relay(ref_input,false);
+  delay(500);
+  start_conversion();
+  delay(500);
+  double reading=samplecount/(((10.0*get_runup_read())+(read_run1())-(read_run2())+(read_run3()/4.1)-(read_run4()/38.0)+(read_run5()/260.0) ));
+  set_relay(zero,false);
+  set_relay(ref_input,true);
+  gain=reading;
   }
+void adc_vref(){
+  set_relay(zero,true);
+  set_relay(ref_input,true);
+  delay(500);
+  start_conversion();
+  delay(500);
+  double reading=((10+ext_zerocal)*samplecount)/(gain*((10.0*get_runup_read())+(read_run1())-(read_run2())+(read_run3()/4.1)-(read_run4()/38.0)+(read_run5()/260.0) ));
+  vref=reading;
+  set_relay(zero,false);
+  set_relay(ref_input,false);
+  }
+void adc_zero(){
+  set_relay(zero,false);
+  delay(500);
+  start_conversion();
+  delay(500);
+  double reading=gain*(((( (10.0*get_runup_read())+(read_run1())-(read_run2())+(read_run3()/4.1)-(read_run4()/38.0)+(read_run5()/260.0) ))/samplecount));
+  zerocal=reading;
+  set_relay(zero,true);
+  }
+void ext_zero(){
+  set_relay(zero,true);
+  set_relay(ref_input,true);
+  delay(500);
+  start_conversion();
+  delay(500);
+  double reading=gain*(((( (10.0*get_runup_read())+(read_run1())-(read_run2())+(read_run3()/4.1)-(read_run4()/38.0)+(read_run5()/260.0) ))/samplecount));
+  ext_zerocal=reading;
+  set_relay(zero,false);
+  set_relay(ref_input,false);
+  }
+
 double reading(double stime){
-  //
-  double reading=gain*(((( (10.0*get_runup_read())+(read_run1()/10.0)-(read_run2()/10.0)+(read_run3()/41.0)-(read_run4()/380.0)+(read_run5()/2600.0) ))/samplecount)-zerocal);
+  double reading=(vref*gain)*(((( (10.0*get_runup_read())+(read_run1())-(read_run2())+(read_run3()/4.1)-(read_run4()/38.0)+(read_run5()/260.0) ))/samplecount))-ext_zerocal;
   return reading;
+  }
+void start_conversion(){
+  delayMicroseconds(500);
+  digitalWrite(conv,HIGH);
+  delayMicroseconds(100);
+  digitalWrite(conv,LOW);
   }
 void test_func(){
     Serial.print("Runup Read: ");
@@ -155,24 +222,41 @@ void setup() {
   pinMode(add_1,OUTPUT);
   pinMode(add_2,OUTPUT);
   pinMode(add_3,OUTPUT);
-
   
+  adc_gain();
+  adc_zero();
+  //ext_zero();
+  adc_vref();
+  
+  set_relay(zero,true);
+  set_relay(ref_input,false);
 }
 
 void loop() {
   
   
   if(digitalRead(data_ready)){
-    //test_func();
-    //Serial.print("Raw Count: ");
-    //Serial.println(raw_reading(stime),12);
-    //Serial.print("Reading: ");
-    Serial.println(reading(stime),7);
+    double new_reading=reading(stime);
+    Serial.print(new_reading,7);
+    Serial.print("\t");
+    average_1(new_reading);
+    if(running_average==10){
+      Serial.print(rolling,7);
+      Serial.print("\t");
+      }
+    else{
+      Serial.print("not average");
+      Serial.print("\t");
+      }
+    Serial.print(vref,10);
+    Serial.print("\t");
+    Serial.print(gain,10);
+    Serial.print("\t");
+    Serial.print(zerocal,10);
+    Serial.print("\t");
+    Serial.println(ext_zerocal,10);
     //Serial.println("Vdc");
-    delayMicroseconds(500);
-    digitalWrite(conv,HIGH);
-    delayMicroseconds(100);
-    digitalWrite(conv,LOW);
+    start_conversion();
     }
     
   if(!digitalRead(data_ready)){
